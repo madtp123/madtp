0704e7513bde146bf4732bb314270852c9dcca80
madtp <m95238548@gmail.com>
2024-05-22 14:03:55 +1000

    credit

--- packages/target_follower/src/target_follower_credit.py
+++ packages/target_follower/src/target_follower_credit.py
@@ -1,41 +1,74 @@
 #!/usr/bin/env python3
 
 import rospy
-import math
-import time
-
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
 
 class Target_Follower:
     def __init__(self):
         # Initialize ROS node
         rospy.init_node('target_follower_node', anonymous=True)
-        rospy.loginfo("Target follower node started.")
 
         # When shutdown signal is received, we run clean_shutdown function
+lf.stop_robot()
+            return
+        self.stop_robot()
+        x = detections[0].transform.translation.x
+        y = detections[0].transform.translation.y
+        z = detections[0].transform.translation.z
+        rospy.loginfo("x, y, z: %f, %f, %f", x, y, z)
+        rospy.sleep(1)
+        if z > 0.15:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0.4
+            cmd_msg.omega = 0
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.2)
+            self.stop_robot()
+        if z < 0.10:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = -0.4
+            cmd_msg.omega = 0
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.2)
+            self.stop_robot()
+        if x > 0.05:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0
+            cmd_msg.omega = 0.4
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.4)
+            self.stop_robot()
+        if x < -0.05:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0
+            cmd_msg.omega = -0.4
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.4)
+            self.stop_robot()
+
+if __name__ == '__main__':
+    try:
+        target_follower = Target_Follower()
+    except rospy.ROSInterruptException:
+        pass
         rospy.on_shutdown(self.clean_shutdown)
 
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "madtp" WITH YOUR ROBOT'S NAME #####
+        # Initialize publisher and subscriber with your robot's name
         self.cmd_vel_pub = rospy.Publisher('/madtp/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/madtp/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
-        ################################################################
-
-        self.object_detected = False
-        self.seek_object_mode = False  # Start in lane following mode
-        self.prev_object_detected = False  # To track the previous object detection state
-        self.stop_sign_detected = False
-        self.stop_sign_wait_start_time = None
-
+        self.tag_sub = rospy.Subscriber('/madtp/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
+        
         rospy.spin() # Spin forever but listen to message callbacks
 
-    # Apriltag Detection Callback
+    # April Tag Detection Callback
     def tag_callback(self, msg):
-        rospy.loginfo("Received message from AprilTag detector node.")
         self.move_robot(msg.detections)
-
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+ 
+    # Stop Robot before node has shut down. This ensures the robot keeps moving with the last velocity command
     def clean_shutdown(self):
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
@@ -49,38 +82,47 @@ class Target_Follower:
         self.cmd_vel_pub.publish(cmd_msg)
 
     def move_robot(self, detections):
-        #### YOUR CODE GOES HERE ####
-        stop_sign_detected = False
-        for detection in detections:
-            if detection.tag_id == 18:  # Stop sign tag ID
-                stop_sign_detected = True
-                break
-
-        if stop_sign_detected:
-            self.stop_sign_detected = True
-            self.stop_sign_wait_start_time = rospy.get_time()
-            print("Stop sign detected, stopping for 5 seconds.")
-        elif self.stop_sign_detected:
-            current_time = rospy.get_time()
-            if current_time - self.stop_sign_wait_start_time >= 5.0:  # Wait for 5 seconds
-                self.stop_sign_detected = False
-                print("Resuming lane following.")
-
-        # Publish velocity commands
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-
-        if self.stop_sign_detected:
-            cmd_msg.v = 0.0  # Stop linear motion
-            cmd_msg.omega = 0.0  # Stop angular motion
-        else:
-            # Lane following behavior
-            cmd_msg.v = 0.3  # Set a constant linear velocity
-            cmd_msg.omega = 0.0  # Set angular velocity to zero for lane following
-
-        self.cmd_vel_pub.publish(cmd_msg)
-        rospy.loginfo("Published velocity command: v=%.2f, omega=%.2f", cmd_msg.v, cmd_msg.omega)
-        #############################
+        if len(detections) == 0:
+            self.stop_robot()
+            return
+        self.stop_robot()
+        x = detections[0].transform.translation.x
+        y = detections[0].transform.translation.y
+        z = detections[0].transform.translation.z
+        rospy.loginfo("x, y, z: %f, %f, %f", x, y, z)
+        rospy.sleep(1)
+        if z > 0.15:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0.4
+            cmd_msg.omega = 0
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.2)
+            self.stop_robot()
+        if z < 0.10:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = -0.4
+            cmd_msg.omega = 0
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.2)
+            self.stop_robot()
+        if x > 0.05:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0
+            cmd_msg.omega = 0.4
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.4)
+            self.stop_robot()
+        if x < -0.05:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0
+            cmd_msg.omega = -0.4
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(0.4)
+            self.stop_robot()
 
 if __name__ == '__main__':
     try:
